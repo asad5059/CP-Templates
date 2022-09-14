40853bb55297567afe6b8b38d692a069a2d84b89
Md Mosleh Uddin <49858741+asad5059@users.noreply.github.com>
2022-09-14 11:26:26 +0600

    Create interactive.cpp

---
+++ interactive.cpp
@@ -0,0 +1,51 @@
+struct Interactor{
+    vector<int> arr;
+    int n;
+    int majorityElement;
+    Interactor(){
+        int zeros = 0;
+        int ones = 0;
+        n = 101;
+        for(int i = 0; i <= 100; i++){
+            arr.pb(getRandomNumber(0, 1));
+        }
+        for(int i = 0; i <= 100; i++){
+            if(arr[i] == 0)
+                zeros++;
+            else
+                ones++;
+        }
+        majorityElement = zeros > ones ? 0 : 1;
+        assert(sz(arr) == n);
+    }
+    int queryHandler(int queryNumber){
+        assert(queryNumber >= 0 && queryNumber < n);
+        if(arr[queryNumber] == majorityElement)
+            return 1;
+        else
+            return 0;
+    }
+};
+
+Interactor it;
+int query(int value){
+    cout << "? " << value << endl;
+    return it.queryHandler(value);
+    // char result;
+    // cin >> result;
+    // return result;
+}
+void solve() {
+    it = Interactor();
+    debug(it.arr);
+    int iterations = 0;
+    while(true){
+        iterations++;
+        int index = getRandomNumber(0, it.n - 1);
+        if(query(index)){
+            cout << index << endl;
+            break;
+        }
+    }
+    debug(iterations)
+}
